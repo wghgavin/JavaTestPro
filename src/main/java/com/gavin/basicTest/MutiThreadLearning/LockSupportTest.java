1489a3a849c564d9bc306fe68aaa4e1ecfe2fa88
pengyuheng <2127234805@qq.com>
2020-05-08 23:10:31 +0800

    新增多线程测试方法

---
+++ src/main/java/com/gavin/basicTest/MutiThreadLearning/LockSupportTest.java
@@ -0,0 +1,28 @@
+package com.gavin.basicTest.MutiThreadLearning;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
+
+public class LockSupportTest {
+    public static void main(String[] args) {
+        Thread t = new Thread(()->{
+            for(int i=1;i<=10;i++){
+                if(i==5){
+                    LockSupport.park();
+                }
+                if(i==8){
+                    LockSupport.park();
+                }
+                System.out.println("执行第"+i+"次");
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+        t.start();
+        LockSupport.unpark(t);
+    }
+}
---
+++ src/main/java/com/gavin/basicTest/MutiThreadLearning/MonitorVectorTest/MyVector.java
@@ -0,0 +1,20 @@
+package com.gavin.basicTest.MutiThreadLearning.MonitorVectorTest;
+
+import java.util.Vector;
+/**
+ * 实现一个容器，提供两个方法add,size,写两个线程
+ * 线程1:添加10个元素到容器
+ * 实时监控元素个数,当个数到达5个，线程2给出提示并结束
+ */
+public class MyVector {
+    Vector<Object> vector;
+    public MyVector(){
+        vector = new Vector<>();
+    }
+    public void add(Object obj){
+        vector.add(obj);
+    }
+    public int size(){
+        return vector.size();
+    }
+}
