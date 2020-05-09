ba815aaec29e85213ad967238017a7ba449244a4
吴国宏 <303295469@qq.com>
2020-05-09 16:05:38 +0800

    新增线程池调用例子

--- src/main/java/com/gavin/basicTest/MutiThreadLearning/MonitorVectorTest/MyVector.java
+++ src/main/java/com/gavin/basicTest/MutiThreadLearning/MonitorVectorTest/MyVector.java
@@ -2,6 +2,7 @@ package com.gavin.basicTest.MutiThreadLearning.MonitorVectorTest;
 
 import java.util.Vector;
 /**
+ * 淘宝面试题:
  * 实现一个容器，提供两个方法add,size,写两个线程
  * 线程1:添加10个元素到容器
  * 实时监控元素个数,当个数到达5个，线程2给出提示并结束
