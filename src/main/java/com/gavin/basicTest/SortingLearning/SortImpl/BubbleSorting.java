e1b8f27191535ff3c5429ebdd480e46c9d0c4e6d
吴国宏 <303295469@qq.com>
2020-05-25 11:09:09 +0800

    排序算法更新，更改注释

--- src/main/java/com/gavin/basicTest/SortingLearning/SortImpl/BubbleSorting.java
+++ src/main/java/com/gavin/basicTest/SortingLearning/SortImpl/BubbleSorting.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 /**
  * 冒泡排序(时间复杂度为O(n2))【稳定型】//从小到大
  * 优化，增加flag，标志没有进行对换，说明早已经排序成功
+ * 存在问题效率低
  */
 @SortMethod("冒泡排序")
 public class BubbleSorting implements ISorting {
